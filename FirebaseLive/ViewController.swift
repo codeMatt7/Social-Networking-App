23b6bc2f9c43df8001e484e8657ebec2cdb013ff
Frezy Mboumba <frezymboumba@Frezys-MacBook-Pro.local>
2017-01-15 21:20:18 +0100

    Initial Commit

---
+++ FirebaseLive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseLive
+//
+//  Created by Frezy Mboumba on 1/15/17.
+//  Copyright © 2017 Frezy Mboumba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
